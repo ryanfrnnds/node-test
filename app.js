f726eb2e95436fa04cd6f63fd44a7d1d6dc64d35
Ryan Fernandes <ryanfrnnds@gmail.com>
2019-06-08 18:47:16 -0300

    Primeiro Commite. Estrutura inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+console.log('Hello world!');
+
+const express = require('express');
+const app = new express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+ 
+app.listen(3000, () => {
+    console.log('Server up!');
+});
